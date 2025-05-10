e15e4fae4b1d38527f2496be18dc61ea150a8a57
Oleksandr Baranov <aleks.bar89@gmail.com>
2025-05-10 16:44:42 +0300

    2.0.1

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Hello World")
-
-print("Hello Git")
+name = input(“What is your name? ”)
+What is your name? Wendy
+[комп'ютер шукає в базі даних ім'я "Венді"]
+“Wendy” ← True — це ім'я є в базі даних
+      print(“Hello, Wendy”)
+“Wendy” ← False Такої назви немає в базі даних
+      print(“Sorry, I don’t know you.”)
