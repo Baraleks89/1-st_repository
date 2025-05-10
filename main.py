c20db7bf3deadd3d0ccc0735439f71f69ea462a1
Oleksandr Baranov <aleks.bar89@gmail.com>
2025-05-10 17:49:48 +0300

    2.0.1

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-name = input(“What is your name? ”)
-What is your name? Wendy
-[комп'ютер шукає в базі даних ім'я "Венді"]
-“Wendy” ← True — це ім'я є в базі даних
-      print(“Hello, Wendy”)
-“Wendy” ← False Такої назви немає в базі даних
-      print(“Sorry, I don’t know you.”)
+print("Hello World!")
+print("This is a test.")
